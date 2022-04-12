0397d52776e5cd11cdafccb5592864b7d9278815
Katy Tseng <100108955+ccaunite@users.noreply.github.com>
2022-04-12 00:35:11 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,22 +1,25 @@
+const mediaQuery = window.matchMedia('(min-width: 768px)');
 
 function navDisplay() {
-  var hamCtn = document.getElementById("hamburger-container");
-  var ham = document.getElementById("nav-hamburger")
-  var x = document.getElementById("nav");
-  var registerBtn = document.getElementById("register-button");
-  if (x.style.display === "none") {
-    hamCtn.classList.add('add-bg');
-    ham.style.color = "#000000";
-    x.style.display = "block";
-    registerBtn.style.backgroundColor = "#0D0A29";
-    registerBtn.style.color = "#ffffff";
-    document.body.style.overflow = "none";
-  } else {
-    hamCtn.classList.remove('add-bg');
-    ham.style.color = "#ffffff";
-    x.style.display = "none";
-    registerBtn.style.backgroundColor = "#ffffff";
-    registerBtn.style.color = "#0D0A29";
-    document.body.style.overflow = "auto";
+  if (mediaQuery.matches) {
+    var hamCtn = document.getElementById("hamburger-container");
+    var ham = document.getElementById("nav-hamburger");
+    var x = document.getElementById("nav");
+    var registerBtn = document.getElementById("register-button");
+    if (x.style.display === "none") {
+      hamCtn.classList.add('add-bg');
+      ham.style.color = "#000000";
+      x.style.display = "block";
+      registerBtn.style.backgroundColor = "#0D0A29";
+      registerBtn.style.color = "#ffffff";
+      document.body.style.overflow = "none";
+    } else {
+      hamCtn.classList.remove('add-bg');
+      ham.style.color = "#ffffff";
+      x.style.display = "none";
+      registerBtn.style.backgroundColor = "#ffffff";
+      registerBtn.style.color = "#0D0A29";
+      document.body.style.overflow = "auto";
+    }
   }
 }
